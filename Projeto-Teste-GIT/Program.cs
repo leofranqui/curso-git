c0106ad3cabc33af833dac8168253ac2c503f767
leofranqui <leomfranqui@hotmail.com>
2019-05-26 17:35:19 -0300

    programa escreve ola mnundo na tela

---
+++ Projeto-Teste-GIT/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Projeto_Teste_GIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String frase = Console.ReadLine();
+            
+
+             Console.WriteLine("Hello World! Voce digitou :" + frase );
+            Console.ReadLine();
+
+        }
+    }
+}
